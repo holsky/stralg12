1517d4e255d3fd60e7ad717f95b60b31ae173f53
holger-s <holger.s@fu-berlin.de>
2013-04-24 13:56:11 +0200

    - slowscan

--- src/stralg13/SuffixTree.java
+++ src/stralg13/SuffixTree.java
@@ -1,18 +1,19 @@
 package stralg13;
 
+
 import java.util.Map;
 
 public class SuffixTree {
 	public static final String STRING_END = "$";
 	Node root;
 	String string;
-	
+
 	public SuffixTree(String string) {
 		this.string = string + STRING_END;
-				
+
 		makeSuffixTree();
 	}
-	
+
 	public SuffixTree() {
 		root = new Node();
 	}
@@ -20,27 +21,44 @@ public class SuffixTree {
 	public void makeSuffixTree() {
 		root = new Node();
 		root.addEdge(0, string.length() - 1);
-	}
-	
-	public boolean equals(SuffixTree otherTree) {
-		return string.equals(otherTree.string) &&
-				root.equals(otherTree.root);
-	}
-	
-	boolean compareString(int startIndex, Tuple tuple)
-	{
-	    return string.substring(tuple.first, tuple.second  - tuple.first)
-	            .equals(string.substring(startIndex, tuple.second - tuple.first));
+		/*Node headOfIplus1 = null;
+		Node headOfI = root;*/
+		for (int i = 0; i < string.length(); ++i) {
+			if (headOfI.equals(root)) {
+				headOfIplus1 = slowscan(root, i);
+				headOfI.addEdge(i+1, string.length() - 1);
+			} else {
+				
+			}
+			
+			headOfI = headOfIplus1;
+		}
 	}
 
-	Node slowscan(Node startNode, int startIndex)
-	{
-	    for (Map.Entry<Tuple, Node> edge : startNode.edges.entrySet()) {
-	        if (compareString(startIndex, edge.getKey())) {
-	           return children[key]->slowscan(startIndex + key.second - key.first);
-	        }
-	    }
-	    return this;
+	public boolean equals(SuffixTree otherTree) {
+		return string.equals(otherTree.string) && root.equals(otherTree.root);
 	}
-	
+
+	boolean containsString(int startIndex, Tuple tuple) {
+		return string.substring(startIndex)
+				.startsWith(string.substring(tuple.first, tuple.second));
+	}
+
+	Node slowscan(Node startNode, int startIndex) {
+		for (Map.Entry<Tuple, Node> edge : startNode.edges.entrySet()) {
+			if (containsString(startIndex, edge.getKey())) {
+				return slowscan(startNode.edges.get(edge.getKey()),
+								startIndex + edge.getKey().second
+										- edge.getKey().first);
+			}
+		}
+		return startNode;
+	}
+
+	public Node head(int i) {
+		
+		
+		return null;
+	}
+
 }
