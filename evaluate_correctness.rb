7a817071a48f52cf62aeef79a4f48dadb1c748ed
Matus Tomlein <matus@tomlein.org>
2013-05-29 13:13:18 +0200

    Update correction evaluation

--- evaluate_correctness.rb
+++ evaluate_correctness.rb
@@ -1,15 +1,5 @@
 class NaiveSearch
   def self.search(string, query)
-    #matches1 = []
-    #i = 0
-    #while i < string.length
-    #  c = string[i..string.length].index(query)
-    #  break if c.nil?
-    #  i += c
-    #  matches1 << i.to_s
-    #  i += 1
-    #end
-
     matches = []
     string.length.times do |i|
       if string[i..(i + query.length - 1)] == query
@@ -23,12 +13,16 @@ end
 
 return unless ARGV.any?
 
-#NaiveSearch.search(File.read(ARGV.first), ARGV.last)
 input = File.read(ARGV.first)
-query = ARGV.last
-naive_matches = NaiveSearch.search(input, query)
-ba_matches = `./search-ba #{ARGV.first} #{query}`.split(' ')
-kmp_matches = `./search-kmp #{ARGV.first} #{query}`.split(' ')
+20.times do |i|
+  q_from = Random.rand(input.length - 17)
+  q_until = q_from + Random.rand(15) + 2
+  query = input[q_from..q_until]
+  puts query
+  naive_matches = NaiveSearch.search(input, query)
+  ba_matches = `./search-ba #{ARGV.first} #{query}`.split(' ')
+  kmp_matches = `./search-kmp #{ARGV.first} #{query}`.split(' ')
 
-puts naive_matches == kmp_matches
-puts naive_matches == ba_matches
+  puts naive_matches == kmp_matches
+  puts naive_matches == ba_matches
+end
